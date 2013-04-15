77723151d21da4eb9ba8ee175c017876dfbaa474
Anti <anti.rasta@khk.ee>
2013-04-15 13:01:41 +0300

    WTF is this.

---
+++ classes/request.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: anti.rasta
+ * Date: 15.04.13
+ * Time: 12:54
+ * To change this template use File | Settings | File Templates.
+ */
+
+class request {
+	public $controller = DEFAULT_CONTROLLER;
+	public $action = 'index';
+	public $params = array();
+
+
+
+}
